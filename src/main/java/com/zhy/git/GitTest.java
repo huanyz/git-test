dd579eac51d00f9c5724a594f5d39c6fa15f99c5
zhy <zhy@qq.com>
2022-05-05 12:02:41 +0800

    first commit

---
+++ src/main/java/com/zhy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zhy.git;
+
+/**
+ * @author zhy
+ * @date 2022/5/5 11:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
